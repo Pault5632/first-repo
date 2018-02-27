1d079aeccd564f802e82b502014b3080eb155034
Pault5632 <36477801+Pault5632@users.noreply.github.com>
2018-02-27 12:47:27 +0000

    Add files via upload

---
+++ ContinueTest.java
@@ -0,0 +1,13 @@
+public class ContinueTest
+{
+ public static void main(String[] args){
+ for (int count = 1; count <= 10; count++){
+ if(count != 5)
+
+
+ System.out.println(count);
+
+ System.out.print("\nUsed continue to skip printing 5");
+   }
+ }
+}
